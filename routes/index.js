0fb406aeacac85c6133453d68fe7c37b0d2994f4
mario <mmadzar@users.noreply.github.com>
2018-05-21 04:36:24 +0200

    documentation, routes cleanup

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// POST method route
-router.post('/sendmessage', function (req, res) {
-	console.log('sendmessage', req.body);
-  res.send(null);
-})
-
-router.post('/startmonitor', function (req, res) {
-	console.log('startmonitor', req.body);
-  res.send(null);
-})
-
-router.post('/stopmonitor', function (req, res) {
-	console.log('stopmonitor', req.body);
-  res.send(null);
-})
-
 module.exports = router;
